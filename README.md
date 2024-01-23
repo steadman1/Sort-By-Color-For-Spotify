54dd2f6998b90debb6d3988abf540fb8071b47dc
steadman1 <steadman@vt.edu>
2024-01-22 21:02:20 -0500

    finished with my color sorting algo!

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # Sort Your Music by Color
+
+## Determining One-Dimensional Color Sorting
+
+### Requirements
+- Sorting must be pleasing to the eye/aesthetic
+- Must be one-dimensional
+
+### Ideas
+1. **Sort By Distance** ➡️ Take an arbitrary [a*, b*] where a is your start and b is your end point—an easy example would be white and black. From there, calculate and sort the distances between each value (r, g, b) it would take to get to the next value. [2]
+2. **Sort By Relative Luminance** ➡️ Using this formula: `let luminance: Double = 0.2126 * r + 0.7152 * g + 0.0722 * b` where (r, g, b) represent given red, green, and blue values. From there, sort the luminance values. [1]
+3. **Sort By Relative Brightness (HSP Color Model)** ➡️ As used in Photoshop, we can employ an RGB-to_Greyscale model to obtain brightness using the following equation: `let brightness: Double = sqrt( .299 * r_sq + .587 * g_sq + .114 * b_sq )` where (r_sq, g_sq, b_sq) represent r, g, b values that have been squared. From there, we can sort the greyscale values. [1] [3]
+4. **Sort By Hue** ➡️ Using the HSL color model, we can convert each (r, g, b) value to a hue value and sort. The full equation is a bit long, but it's pretty easy to find, so you can find it in the fourth citation. [4]
+
+### Testing
+Wow, this is pretty difficult. From testing with various functions, I found the hue sorting to be by far the best, however, it doesn't take into account brightness, so holistically it looks great, but with a small list of colors, the discontinuities show a LOT more.
+- ⬆️ maybe I can sort by hue and then brightness within each hue subset and make the discontinuities more artistic or something (???)
+- ⬆️⬆️ Like the jumps between hues/colors would be so noticeable that it's like just separation or something.
+- ⬆️ The discontinuities might not even be that noticeable either way since, at the end of the day, this is sorting album colors as a whole, not individual colors.
+
+### Final Thoughts / Conclusion
+Hue sorting was definitely the move, and I was able to accomplish exactly what I wanted to albeit with a slight hack. Here's the plain text explanation of my algorithm
+1. First, I group each hue into a list of colors given by a semi-arbitrary range of max and min values for said color: e.g. reds from 0 to 0.083, yellows from 0.083 to 0.25, and so on.
+2. Second, within each of the subsets of colors, I sort by greyscale (luminance would also work here)
+3. Next, I use a step to grab half of each of the colors starting from 0 and 1 respectively. This essentially allowed me to create 2 lists of colors both going from light to dark.
+4. Finally, I reverse the second list and combine the two lists into one making the combination go from light to dark and back to light allowing the colors, at least in a perfect world, to perfectly match between colors as they blend from as close to white as possible of one hue to as close to white as possible of another hue.
+
+### Remarks
+ - I used Swift 5 syntax for the pseudocode parts as I'm most familiar with it despite this project being in Python (sorry for that)
+
+### Resources / Citations
+- [1] https://stackoverflow.com/questions/3014402/sorting-a-list-of-colors-in-one-dimension
+- [2] https://en.wikipedia.org/wiki/Dimensionality_reduction#:~:text=Dimensionality%20reduction%2C%20or%20dimension%20reduction,close%20to%20its%20intrinsic%20dimension.
+- [3] https://alienryderflex.com/hsp.html
+- [4] https://stackoverflow.com/questions/23090019/fastest-formula-to-get-hue-from-rgb
--- lib/sort_colors.py
+++ lib/sort_colors.py
@@ -1,24 +1,147 @@
+import pygame
+from math import sqrt
+import numpy as np
+from colormath.color_objects import LabColor, LCHabColor
+from colormath.color_conversions import convert_color
+from scipy.spatial.distance import pdist, squareform
+from scipy.optimize import linear_sum_assignment
+from itertools import groupby
 
-rgb_colors = [
-    (255, 0, 0),    # Red
-    (0, 255, 0),    # Green
-    (0, 0, 255),    # Blue
-    (255, 255, 0),  # Yellow
-    (0, 255, 255),  # Cyan
-    (255, 0, 255),  # Magenta
-    (192, 192, 192),# Silver
-    (128, 0, 0),    # Maroon
-    (128, 128, 0),  # Olive
-    (0, 128, 0),    # Dark Green
-    (128, 0, 128),  # Purple
-    (0, 128, 128),  # Teal
-    (0, 0, 128),    # Navy
-    (255, 165, 0),  # Orange
-    (255, 192, 203),# Pink
-    (105, 105, 105),# Dim Gray
-    (0, 0, 0),      # Black
-    (255, 255, 255) # White
-]
 
-def sort_colors(colors):
+def init_color_display(colors):
+    pygame.init()
+
+    # Screen dimensions and setup
+    screen_width = 1200
+    screen_height = 200
+    screen = pygame.display.set_mode((screen_width, screen_height))
+    pygame.display.set_caption("Color Display")
+
+    color_width = screen_width // len(colors)
+
+    # Main loop
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        # Fill the screen with a white background
+        screen.fill((255, 255, 255))
+
+        # Draw the color rectangles
+        for i, color in enumerate(colors):
+            pygame.draw.rect(screen, pygame.Color(color), (i * color_width, 0, color_width, screen_height))
+
+        # Update the display
+        pygame.display.flip()
+
+    # Clean up
+    pygame.quit()
+
+
+def sort_by_color_distance(colors):
+    colors = np.array(colors) / 255.0
+
+    def color_distance(c1, c2):
+        return np.linalg.norm(np.array(c1) - np.array(c2))
+
+    dist_matrix = squareform(pdist(colors, lambda u, v: color_distance(u, v)))
+
+    row_index, col_ind = linear_sum_assignment(dist_matrix)
+
+    print(row_index)
+
+    return [tuple(int(c * 255) for c in color) for color in colors[col_ind]]
+
+def sort_by_luminance(colors):
+    luminances = []
+    for color in colors:
+        luminance = 0.2126 * color[0] + 0.7152 * color[1] + 0.0722 * color[2]
+        luminances.append(luminance)
+    return [color for (_, color) in sorted(zip(luminances, colors), reverse=True)]
+
+def sort_by_greyscale(colors):
+
+    brightnesses = []
+    for color in colors:
+        brightness = sqrt(0.299 * pow(color[0], 2) + 0.587 * pow(color[1], 2) + 0.114 * pow(color[2], 2)) 
+        brightnesses.append(brightness)
+    return [color for (_, color) in sorted(zip(brightnesses, colors), reverse=True)]
+
+def get_hue(color):
+    r = color[0] / 255
+    g = color[1] / 255
+    b = color[2] / 255
+    cmax = max(r, g, b)
+    cmin = min(r, g, b)
+    delta = cmax - cmin
+    if delta == 0:
+        hue = 0
+    elif cmax == r:
+        hue = 60 * (((g - b) / delta) % 6)
+    elif cmax == g:
+        hue = 60 * (((b - r) / delta) + 2)
+    elif cmax == b:
+        hue = 60 * (((r - g) / delta) + 4)
+
+    if hue > 360:
+        print(hue)
+
+    return hue
+
+def sort_by_hue(colors):
+    hues = []
+    for color in colors:
+        hues.append(get_hue(color))
+        
+    return [color for (_, color) in sorted(zip(hues, colors))]
+
+def sort_by_rainbow(colors):
+    hue_ranges = {
+        'red': [(0.0, 0.083), (0.917, 1.001)],
+        'yellow': [(0.083, 0.25)],
+        'green':[(0.25, 0.430)],
+        'cyan': [(0.430, 0.583)],
+        'blue': [(0.583, 0.75)],
+        'magenta': [(0.75, 0.917)]
+    }
+
+    categorized_colors = {color: [] for color in hue_ranges}
+
+    for color in colors:
+        hue = get_hue(color) / 360
+        for rainbow_color, hue_range in hue_ranges.items():
+            for (low, high) in hue_range:
+                if low <= hue < high:
+                    categorized_colors[rainbow_color].append(color)
+                    break
+
+    straight_colors = []
+
+    for hue_range in hue_ranges:
+        sorted_hue = sort_by_greyscale(categorized_colors[hue_range])
+        head = sorted_hue[0::2]
+        tail = sorted_hue[1::2]
+        straight_colors += head + tail[::-1]
+
+    return straight_colors
     
+
+if __name__ == '__main__':
+    color_count = 600
+    for i in range(1):
+        rgb_colors = np.random.rand(color_count, 3) * 255.0  # 100 random colors
+
+        colors = sort_by_rainbow(rgb_colors)
+
+        print(len(colors))
+
+        # if len(colors) != color_count:
+        #     difference = []
+        #     for color in rgb_colors:
+        #         if not np.array(color) in np.array(colors):
+        #             difference.append(color)
+
+        #     print(difference)
+        init_color_display(colors)
